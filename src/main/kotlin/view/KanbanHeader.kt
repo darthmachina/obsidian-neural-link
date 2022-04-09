b4c541017a2d70271174c88c510caf9cf5f535e1
darthmachina <github@outerrim.org>
2022-04-08 22:46:56 -0500

    Added header component

--- src/main/kotlin/view/KanbanHeader.kt
+++ src/main/kotlin/view/KanbanHeader.kt
@@ -1,4 +1,16 @@
 package view
 
-class KanbanHeader {
+import io.kvision.core.AlignItems
+import io.kvision.core.JustifyContent
+import io.kvision.html.div
+import io.kvision.panel.HPanel
+import model.TaskModel
+import org.reduxkotlin.Store
+
+class KanbanHeader(val store: Store<TaskModel>) : HPanel(spacing = 5, justify = JustifyContent.END) {
+    init {
+        addCssStyle(KanbanStyles.KANBAN_HEADER)
+        div { +"Tag Filter" }
+        div { +"Page Filter" }
+    }
 }
