7b9767e863bd8426bec7c95c8bcefdc8ecf3faa4
Michael Osterlie <michael.osterlie@nexteraanalytics.com>
2022-05-09 09:55:17 -0500

    Fixed chained log function parameter call

--- src/main/kotlin/neurallink/core/service/UtilFunctions.kt
+++ src/main/kotlin/neurallink/core/service/UtilFunctions.kt
@@ -4,7 +4,7 @@ import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
 
-fun <A> A.log(marker: String, msg: (A) -> Any? ): A {
-    logger.debug { "$marker ${msg.invoke(it)}" }
+fun <A> A.log(marker: String, f: () -> Any? ): A {
+    logger.debug { "$marker: ${f()}" }
     return this
 }
