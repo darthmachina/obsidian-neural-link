81a041d3d7dde48967c04746f49f77d5a4697c3c
Michael Osterlie <michael.osterlie@nexteraanalytics.com>
2022-07-08 11:50:59 -0500

    Added tests for main markdownToStyle method and fixed italic regex for combined bold and italic text

--- src/main/kotlin/neurallink/core/service/MarkdownFunctions.kt
+++ src/main/kotlin/neurallink/core/service/MarkdownFunctions.kt
@@ -2,9 +2,9 @@ package neurallink.core.service
 
 import neurallink.core.model.Task
 
-val WIKILINK_REGEX = Regex("""\[\[([A-Za-z]+)\]\]""")
-val BOLD_REGEX = Regex("""\*\*([A-Za-z]+)\*\*""")
-val ITALIC_REGEX = Regex("""\*([A-Za-z]+)\*""")
+val WIKILINK_REGEX = Regex("""\[\[([A-Za-z ]+)\]\]""")
+val BOLD_REGEX = Regex("""\*\*([A-Za-z ]+)\*\*""")
+val ITALIC_REGEX = Regex("""\*([A-Za-z <>"=/-]+)\*""")
 
 /**
  * Creates a Markdown String, suitable for writing to a Markdown file.
--- src/test/kotlin/neurallink/view/ViewFunctionsTest.kt
+++ src/test/kotlin/neurallink/view/ViewFunctionsTest.kt
@@ -31,4 +31,61 @@ class ViewFunctionsTest : StringSpec({
         val actualText = markdownLinkToStyle(testText)
         actualText shouldBe expectedText
     }
+
+    // markdownToStyle()
+    "markdownToStyle works with just bold" {
+        val testText = "Test **bold** text"
+        val expectedText = "Test <span class=\"nl-bold\">bold</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with just italics" {
+        val testText = "Test *italic* text"
+        val expectedText = "Test <span class=\"nl-italic\">italic</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with just a link" {
+        val testText = "Test [[link]] text"
+        val expectedText = "Test <span class=\"nl-wikilink\">link</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with separate bold and italic" {
+        val testText = "Test **bold** and *italic* text"
+        val expectedText = "Test <span class=\"nl-bold\">bold</span> and <span class=\"nl-italic\">italic</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with combined bold and italic" {
+        val testText = "Test ***italic and bold*** text"
+        val expectedText = "Test <span class=\"nl-italic\"><span class=\"nl-bold\">italic and bold</span></span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with bold and a link" {
+        val testText = "Test [[link]] and **bold** text"
+        val expectedText = "Test <span class=\"nl-wikilink\">link</span> and <span class=\"nl-bold\">bold</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
+
+    "markdownToStyle works with italic and a link" {
+        val testText = "Test [[link]] and *italic* text"
+        val expectedText = "Test <span class=\"nl-wikilink\">link</span> and <span class=\"nl-italic\">italic</span> text"
+
+        val actualText = markdownToStyle(testText)
+        actualText shouldBe expectedText
+    }
 })
